c7e63491e84505db3ff8d595e1e8da4db4a8136d
Benjamin May <benpmay24@gmail.com>
2024-08-03 18:21:33 -0700

    second commit

--- little_lemon_api/settings.py
+++ little_lemon_api/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'little_lemon_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'little_lemon_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': 'Ramsey040521!',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
